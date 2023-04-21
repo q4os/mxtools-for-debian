ce46dee2288021ae025281e1e57218bf0f03f789
Q4OS Team <info@q4os.org>
2023-04-21 14:43:26 +0200

    Update build script

--- build_all.sh
+++ build_all.sh
@@ -16,16 +16,24 @@ read -p "Prepare for build, press Enter to continue ..." XYZ
 rm -rf $MX_BUILD_DIR
 rsync -a --exclude '.git/' --exclude '.git/' . $MX_BUILD_DIR/
 find $MX_BUILD_DIR/ -maxdepth 1 -type f -delete
-mkdir -p $MX_BUILD_DIR/.logs/
 
 echo
 read -p "Build all packages, press Enter to continue ..." XYZ
 cd $MX_BUILD_DIR/
+mkdir -p $MX_BUILD_DIR/.logs/
 for SRCDIR1 in * ; do
   echo
   echo "Building in : $SRCDIR1/"
   cd $MX_BUILD_DIR/$SRCDIR1/
   dpkg-buildpackage -rfakeroot -uc -b 2>&1 | tee $MX_BUILD_DIR/.logs/${SRCDIR1}.log
 done
+echo "Saving packages ..."
 mkdir -p $MX_BUILD_DIR/.builtout/
 mv $MX_BUILD_DIR/*.deb $MX_BUILD_DIR/.builtout/
+echo "Creating lists ..."
+mkdir -p $MX_BUILD_DIR/.lists/
+ls -1 $MX_BUILD_DIR/.builtout/ | awk -F'_' '{ print $1" "$2 }' | sort > $MX_BUILD_DIR/.lists/list_pcks1.lst
+ls -1 $MX_BUILD_DIR/.builtout/ | awk -F'_' '{ print $1 }' | sort > $MX_BUILD_DIR/.lists/list_pcks2.lst
+
+echo
+echo "Debian packages has been saved: \"$MX_BUILD_DIR/.builtout/\""
